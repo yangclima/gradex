7ee44a4f23f8f825d0f73b5cb67be1dcef8b5cc2
yangclima <yangclima@gmail.com>
2025-04-24 19:31:36 -0300

    add project constants file

---
+++ utils/constants.js
@@ -0,0 +1,87 @@
+export const WEEK_DAYS = {
+  1: { en: "Sunday", pt: "Domingo" },
+  2: { en: "Monday", pt: "Segunda" },
+  3: { en: "Thuesday", pt: "Terça" },
+  4: { en: "Wednesday", pt: "Quarta" },
+  5: { en: "Thursday", pt: "Quinta" },
+  6: { en: "Friday", pt: "Sexta" },
+  7: { en: "Saturday", pt: "Sábado" },
+
+  ALL: {
+    en: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+
+    pt: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+  },
+};
+
+export const ABBREVIATED_WEEK_DAYS = {
+  1: { en: "Sun", pt: "Dom" },
+  2: { en: "Mon", pt: "Seg" },
+  3: { en: "Tus", pt: "Ter" },
+  4: { en: "Wed", pt: "Qua" },
+  5: { en: "Thu", pt: "Qui" },
+  6: { en: "Fri", pt: "Sex" },
+  7: { en: "Sat", pt: "Sáb" },
+
+  ALL: {
+    en: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+
+    pt: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+  },
+};
+
+export const SHEDULES = {
+  M: [
+    "07:00-07:50",
+    "08:00-08:50",
+    "09:00-09:50",
+    "10:00-10:50",
+    "11:00-11:50",
+  ],
+
+  T: [
+    "12:00-12:50",
+    "13:00-13:50",
+    "14:00-14:50",
+    "15:00-15:50",
+    "16:00-16:50",
+    "17:00-17:50",
+  ],
+
+  N: [
+    "18:00-18:50",
+    "18:50-19:40",
+    "19:40-20:30",
+    "20:30-21:20",
+    "21:20-22:10",
+    "22:10-23:00",
+  ],
+
+  ALL: [
+    "07:00-07:50",
+    "08:00-08:50",
+    "09:00-09:50",
+    "10:00-10:50",
+    "11:00-11:50",
+    "12:00-12:50",
+    "13:00-13:50",
+    "14:00-14:50",
+    "15:00-15:50",
+    "16:00-16:50",
+    "17:00-17:50",
+    "18:00-18:50",
+    "18:50-19:40",
+    "19:40-20:30",
+    "20:30-21:20",
+    "21:20-22:10",
+    "22:10-23:00",
+  ],
+};
