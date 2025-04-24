1b921f6fa432e3fc4fcd69d9f7f82975fb95d8aa
yangclima <yangclima@gmail.com>
2025-04-24 16:29:20 -0300

    install and configure tailwindcss

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// These styles apply to every route in the application
+import "styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
